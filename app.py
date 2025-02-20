299fe2b9e8a635d906375cfeb692a5fb4b5675e6
Riccardo Cambio <caoticaproject@gmail.com>
2025-02-20 23:41:59 +0100

    Initial commit - Uffici App

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import psycopg2
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ricambio:a07Rnun4mRgb3fHrCrayH1KM62HFDDqV@dpg-curqd0gfnakc73erqm20-a.frankfurt-postgres.render.com/uffici_db_w4w1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'supersegreto'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class Utente(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    ruolo = db.Column(db.String(50), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Utente.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.form
+        hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+        new_user = Utente(username=data['username'], email=data['email'], password_hash=hashed_password, ruolo='utente')
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form
+        user = Utente.query.filter_by(username=data['username']).first()
+        if user and bcrypt.check_password_hash(user.password_hash, data['password']):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        return "Credenziali non valide", 401
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
+
+
+
